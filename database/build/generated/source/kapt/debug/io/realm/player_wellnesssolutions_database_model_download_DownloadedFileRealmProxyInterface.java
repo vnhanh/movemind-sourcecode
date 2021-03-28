f8a93786200e1841947501f29e78c3cb0da192b5
vnhanh <nghiamy15@gmail.com>
2021-03-28 18:42:24 +0700

    remove git tracking for build folders

--- database/build/generated/source/kapt/debug/io/realm/player_wellnesssolutions_database_model_download_DownloadedFileRealmProxyInterface.java
+++
@@ -1,7 +0,0 @@
-package io.realm;
-
-
-public interface player_wellnesssolutions_database_model_download_DownloadedFileRealmProxyInterface {
-    public int realmGet$videoId();
-    public void realmSet$videoId(int value);
-}
--- database/build/generated/source/kapt/debug/io/realm/player_wellnesssolutions_database_model_video_RealmLanguageRealmProxyInterface.java
+++
@@ -1,9 +0,0 @@
-package io.realm;
-
-
-public interface player_wellnesssolutions_database_model_video_RealmLanguageRealmProxyInterface {
-    public Integer realmGet$id();
-    public void realmSet$id(Integer value);
-    public String realmGet$name();
-    public void realmSet$name(String value);
-}
